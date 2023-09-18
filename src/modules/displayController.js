6367b567b1cd74f8ca80c7907bd73202201825ce
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-09-18 20:09:20 +0530

    feat: add new project button to add projects to sidebar

--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -1,26 +1,58 @@
 import Project from "./project";
 
 export default class displayController {
-  // static loadHomepage() {
-  //   const sidebar = document.createElement('div');
-  //   sidebar.classList.add('sidebar');
-  //   document.body.appendChild(sidebar);
+  static loadHomePage() {
+    this.openProject('Inbox');
+    this.initProjectButtons();
+  }
 
-  //   const project = document.createElement('div');
-  //   project.classList.add('project');
-  //   document.body.appendChild(project);
+  static openProject(projectName) {
+    const projectNameDest = document.querySelector('.project-name');
+    projectNameDest.textContent = projectName;
+  }
 
-  //   const header = document.createElement('h1');
-  //   header.textContent = 'Todo List';
-  //   header.classList.add('heading');
-  //   project.appendChild(header);
+  static initProjectButtons() {
+    const projectButtons = document.querySelectorAll('.project-button');
+    const newProjectButton = document.querySelector('.new-project');
 
-  //   const projectName = document.createElement('div');
-  //   project.appendChild(projectName);
+    projectButtons.forEach((projectButton) => projectButton.addEventListener('click',this.handleProjectButtons));
+    newProjectButton.addEventListener('click', this.handleNewProjectButton);
+  }
 
+  static handleProjectButtons(e) {
+    displayController.openProject(this.textContent);
+  }
+  
+  static handleNewProjectButton(e) {
+    
+    let input = document.createElement('input');
+    let saveButton = document.createElement('button');
+    let removeButton = document.createElement('button');
+    let containerDiv = document.createElement('div');
+    
+    input.type = 'text';
+    saveButton.textContent = 'Save';
+    removeButton.textContent = 'Remove';
 
-  //   const tempProject = new Project('temporary');
-  //   projectName.textContent = tempProject.getName();
+    containerDiv.classList.add('container');
+    
+    containerDiv.append(input,saveButton,removeButton);
+    document.getElementById('projects-container').appendChild(containerDiv);    
+    
+    saveButton.addEventListener('click', function() {
+      let text = input.value;
+      let outputDiv = document.createElement('div');
+      outputDiv.textContent = text;
+      containerDiv.replaceWith(outputDiv);
+      outputDiv.className = 'sidebar-user-project project-button';
+      outputDiv.addEventListener('click',displayController.handleProjectButtons)
+    });
+    
+    removeButton.addEventListener('click', function() {
+      containerDiv.remove();
+    }); 
+  }
 
-  // }
+ 
+
 }
